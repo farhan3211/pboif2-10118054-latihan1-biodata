e57b547c6055d68c918ff64eac5575d698d77594
farhan3211 <55830838+farhan3211@users.noreply.github.com>
2019-09-26 15:50:06 +0700

    Add files via upload

---
+++ src/pboif2/pkg10118054/latihan1/biodata/Pboif210118054Latihan1Biodata.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118054.latihan1.biodata;
+
+/**
+ *
+ * @author ASUS
+ * nama  :Farhan Milardi
+ * kelas :IF-2
+ * nim   :10118054
+ */
+public class Pboif210118054Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "---------------------");
+        System.out.println("NAMA\t\t Farhan Milardi");
+        System.out.println("ALAMAT\t\t: Jalan Depok 3 no 20");
+        System.out.println("TEMPAT LAHIR\t: Bekasi");
+        System.out.println("TANGGAL LAHIR\t: 04 maret 2000");
+        System.out.println("---------------------");
+        System.out.println("HOBI\t\t: traveling & musik");
+        System.out.println("MAKES\t\t: Nasi");
+        System.out.println("MIKES\t\t: Air");
+       
+        
+    
+    
+
